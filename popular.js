483ce421cdc97e3424e5c885e8a17943646595ea
melissajstudent <missyj2007@gmail.com>
2018-01-21 20:13:44 -0600

    Initial commit

---
+++ popular.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+getPopular();
+});
+
+function getPopular(){
+  $.ajax({
+    url: 'https://api.themoviedb.org/3/discover/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29&',
+    language:'en-US',
+    sort_by: 'popularity.desc',
+    success: function(data) {
+      // console.log(data);
+      var i = 0;
+      $.each(data, function () {
+        console.log(data);
+        $('#movieData').after("<div class='row'><div class='col-md-7'<a href='#'><img class='img-fluid rounded mb-3 mb-md-0' src='https://image.tmdb.org/t/p/w500" + data.results[i].backdrop_path + "' /></a></div><div class='col-md-5'><h3>" + data.results[i].name + "</h3><p>" + data.results[i].overview + "</p><p><strong>Average Voter Rating</strong>: " + data.results[i].vote_average + "</p></div></div><br /><hr />");
+        i++;
+      });
+    }
+  });
+};
