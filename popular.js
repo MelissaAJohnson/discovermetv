258da7a331017e37d1ea7134a34f53f39cd4b11a
melissajstudent <missyj2007@gmail.com>
2018-01-23 23:18:41 -0600

    Added Search

--- popular.js
+++ popular.js
@@ -1,20 +1,27 @@
 $(document).ready(function() {
 getPopular();
+search();
 });
 
 function getPopular(){
   $.ajax({
-    url: 'https://api.themoviedb.org/3/discover/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29&',
+    url: 'https://api.themoviedb.org/3/discover/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29',
     language:'en-US',
     sort_by: 'popularity.desc',
     success: function(data) {
-      // console.log(data);
       var i = 0;
       $.each(data, function () {
-        console.log(data);
+        // console.log(data);
         $('#movieData').after("<div class='row'><div class='col-md-7'<a href='#'><img class='img-fluid rounded mb-3 mb-md-0' src='https://image.tmdb.org/t/p/w500" + data.results[i].backdrop_path + "' /></a></div><div class='col-md-5'><h3>" + data.results[i].name + "</h3><p>" + data.results[i].overview + "</p><p><strong>Average Voter Rating</strong>: " + data.results[i].vote_average + "</p></div></div><br /><hr />");
         i++;
       });
     }
   });
 };
+
+function search() {
+  var input = document.getElementById('searchInput').value;
+  if(event.keyCode == 13) {
+    window.location = 'results.html?search=' + input;
+  }
+}
---
+++ search.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+  var url = document.location.href,
+      params = url.split('?')[1].split('&'),
+      data = {}, tmp;
+  for (var i = 0, l = params.length; i < l; i++) {
+       tmp = params[i].split('=');
+       data[tmp[0]] = tmp[1];
+  }
+
+  $.ajax({
+    url: 'https://api.themoviedb.org/3/search/tv?page=1&query=' + data.search + '&api_key=fb6a1d3f38c3d97f67df6d141f936f29',
+    language: 'en-US',
+    success: function(response) {
+      var i = 0;
+      $.each(response, function () {
+        $('#searchResults').after("<div class='row'><div class='col-md-7'<a href='#'><img class='img-fluid rounded mb-3 mb-md-0' src='https://image.tmdb.org/t/p/w500" + response.results[i].backdrop_path + "' /></a></div><div class='col-md-5'><h3>" + response.results[i].name + "</h3><p>" + response.results[i].overview + "</p><p><strong>Average Voter Rating</strong>: " + response.results[i].vote_average + "</p></div></div><br /><hr />");
+        i++;
+      });
+    }
+  });
+}
+
+function search() {
+  var input = document.getElementById('searchInput').value;
+  if(event.keyCode == 13) {
+    window.location = 'results.html?search=' + input;
+  }
+}
